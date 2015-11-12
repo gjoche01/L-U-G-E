3db49a798dc0ead1c1098cde13d67e7667bb9318
jslavitz <jslavitz@gmail.com>
2015-11-11 19:28:22 -0500

    Prototype Game

---
+++ L-U-G-E/Assets/BoardCharacter.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardCharacter : MonoBehaviour {
+
+	// Use this for initialization
+
+	public Transform RightBound;
+	public Transform LeftBound;
+	public float MAX_ROTATION = 45;
+	public float speed = 1;
+
+	private float cameraOffset;
+	private float rightBound;
+	private float leftBound;
+
+	private Transform mainCamera;
+
+	void Start () {
+		leftBound = LeftBound.position.x;
+		rightBound = RightBound.position.x;
+		mainCamera = Camera.main.transform;
+		cameraOffset = mainCamera.position.y - transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 position = transform.position;
+		Vector3 direction = new Vector3 (Input.GetAxis ("Horizontal"), 0, 0);
+
+		transform.Translate (Vector3.up * speed* Time.deltaTime);
+
+		if ((position.x > leftBound && direction.x < 0) || (direction.x > 0 && position.x < rightBound)) {
+			transform.Translate (direction, Space.World);
+			transform.Rotate(Vector3.up * direction.x * MAX_ROTATION);
+		}
+
+		RaycastHit hit = new RaycastHit ();
+
+		if (Physics.Raycast (position, Vector3.forward, out hit, 2, 1 << 12)) { 
+			Vector3 newPosition = transform.position;
+			newPosition.z = hit.point.z - .2f;
+			transform.position = newPosition;
+		}
+
+		Vector3 camPos = new Vector3(0, transform.position.y + cameraOffset, mainCamera.position.z);
+		mainCamera.position = camPos;
+	} 
+}
---
+++ L-U-G-E/Assets/Sprites/Snow.shader
@@ -0,0 +1,40 @@
+﻿Shader "Custom/Snow" {
+Properties {
+	_Color ("Main Color", Color) = (1,1,1,1)
+	_SSColor ("SS Color", Color) = (1,1,1,1)
+	_MainTex ("Base (RGB)", 2D) = "white" {}
+	_BumpMap ("Normalmap", 2D) = "bump" {}
+}
+
+SubShader {
+	Tags { "RenderType"="Opaque" }
+	LOD 300
+
+CGPROGRAM
+#pragma surface surf Lambert finalcolor:mycolor
+
+sampler2D _MainTex;
+sampler2D _BumpMap;
+fixed4 _Color;
+fixed4 _SSColor;
+
+struct Input {
+	float2 uv_MainTex;
+	float2 uv_BumpMap;
+};
+
+void mycolor (Input IN, SurfaceOutput o, inout fixed4 color){
+          color += _SSColor;
+}
+
+void surf (Input IN, inout SurfaceOutput o) {
+	fixed4 c = tex2D(_MainTex, IN.uv_MainTex) * _Color;
+	o.Albedo = c.rgb;
+	o.Alpha = c.a;
+	o.Normal = UnpackNormal(tex2D(_BumpMap, IN.uv_BumpMap));
+}
+ENDCG  
+}
+
+FallBack "Legacy Shaders/Diffuse"
+}
